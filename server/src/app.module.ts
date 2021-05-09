15612c6a12d719b375797347b6f3c87561f56bf0
pp <phuongthephung@gmail.com>
2021-05-10 01:05:39 +0700

    deploy: custom port, nestjs config, dockerfile

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module, HttpModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GotService } from './got.service';
@@ -6,9 +7,15 @@ import { MoviesService } from './movies.service';
 import { StringService } from './string.service';
 
 import { MoviesController } from './movies.controller';
+import configuration from './../config/configuration';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    HttpModule,
+  ],
   controllers: [AppController, MoviesController],
   providers: [AppService, MoviesService, StringService, GotService],
 })
